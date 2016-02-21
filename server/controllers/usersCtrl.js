0a338266291f50eff6e30292c56eece3bbc441f8
Doug Alderman <agnadcp-d1@yahoo.com>
2016-02-20 19:09:05 -0700

    Change to usersCtrl.js for encrypting password

--- server/controllers/usersCtrl.js
+++ server/controllers/usersCtrl.js
@@ -8,7 +8,8 @@ module.exports = {
         console.log('in create');
         console.log('req.body = ', req.body);
       
-        var newUser = new usersModel(req.body)
+        var newUser = new usersModel(req.body);
+        newUser.password = newUser.generateHash(newUser.password);
         newUser.save(function(err, result) {
             if (err)
                 return res.status(500).send(err);
